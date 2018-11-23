ea54ffd0379928f924045a9fe6a37734d1a31a43
vdorchan <vdorchan@gmail.com>
2018-11-23 16:57:25 +0800

    refactor: modified some sytax error

--- index.js
+++ index.js
@@ -31,150 +31,139 @@ exports.init = function ({ site }) {
   }
 }
 
-const verifyUser = function (user) {
-  return new Promise(async function (resolve, reject) {
-    if (!isLeagelSite(CONFIG.site)) {
-      return reject(new Error('Unable to get site, please use init first!'))
-    }
+const verifyUser = async function (user) {
+  if (!isLeagelSite(CONFIG.site)) {
+    throw new Error('Unable to get site, please use init first!')
+  }
 
-    Object.assign(CONFIG, user)
+  Object.assign(CONFIG, user)
 
-    const postContent = {
-      app: 'upload_' + CONFIG.site,
-      return: `${CONFIG.uploadBaseUrl}/Security?dispatch=login`,
-      username: CONFIG.username,
-      password: CONFIG.password
-    }
+  const postContent = {
+    app: 'upload_' + CONFIG.site,
+    return: `${CONFIG.uploadBaseUrl}/Security?dispatch=login`,
+    username: CONFIG.username,
+    password: CONFIG.password
+  }
 
-    const getSession = async location => {
-      let sessionRes
-      try {
-        sessionRes = await got.post(location)
-      } catch (err) {
-        sessionRes = err
-      }
-
-      return sessionRes.headers['set-cookie'][0].match(/JSESSIONID=\S+;/g)[0].replace(';', '')
-    }
-
-    console.log('验证用户...')
-
-    let authRes
-    let session
-
+  const getSession = async location => {
+    let sessionRes
     try {
-      authRes = await got.post('https://auth.pconline.com.cn/security-server/auth.do', {
-        form: true,
-        body: postContent
-      })
+      sessionRes = await got.post(location)
     } catch (err) {
-      authRes = err
+      if (err.statusCode === 302) {
+        sessionRes = err
+      } else {
+        throw err
+      }
     }
 
-    const {
-      location
-    } = authRes.headers
+    return sessionRes.headers['set-cookie'][0].match(/JSESSIONID=\S+;/g)[0].replace(';', '')
+  }
 
-    if (!location) {
-      reject(new Error('账号不正常，请确认账号能否正常使用！'))
-      return false
-    }
+  console.log('验证用户...')
 
-    const {
-      query
-    } = url.parse(location)
-    const {
-      st
-    } = querystring.parse(query)
-    if (parseInt(st) === -1) {
-      reject(new Error('用户密码错误，请检查配置文件！'))
-    } else {
-      session = await getSession(location)
-      uploadClient = uploadClient.extend({
-        headers: {
-          cookie: session
-        }
-      })
-      console.log('验证成功')
-      resolve(session)
-    }
+  const { headers: { location }, statusCode } = await got.post('https://auth.pconline.com.cn/security-server/auth.do', {
+    form: true,
+    body: postContent,
+    throwHttpErrors: false
   })
+
+  if (!location) {
+    throw new Error(`账号不正常，请确认账号能否正常使用！Response code ${statusCode}`)
+  }
+
+  const {
+    query
+  } = url.parse(location)
+  const {
+    st
+  } = querystring.parse(query)
+  if (parseInt(st) === -1) {
+    throw new Error('用户密码错误，请检查配置文件！')
+  } else {
+    const session = await getSession(location)
+    uploadClient = uploadClient.extend({
+      headers: {
+        cookie: session
+      }
+    })
+    console.log('验证成功')
+    return session
+  }
 }
 
-exports.upload = function (file, opts, session) {
-  return new Promise(async function (resolve, reject) {
-    if (!isLeagelSite(CONFIG.site)) {
-      return reject(new Error('Unable to get site, please use init first!'))
-    }
+exports.upload = async function (file, opts, session) {
+  if (!isLeagelSite(CONFIG.site)) {
+    throw new Error('Unable to get site, please use init first!')
+  }
 
-    Object.assign(CONFIG, opts)
+  Object.assign(CONFIG, opts)
 
-    let {
-      targetPath
-    } = opts
+  let {
+    targetPath
+  } = opts
 
-    if (!session) {
-      await verifyUser(opts.user)
-    }
+  if (!session) {
+    await verifyUser(opts.user)
+  }
 
-    if (!targetPath) {
-      return reject(new Error('Illegal path!'))
-    }
+  if (!targetPath) {
+    throw new Error('Illegal path!')
+  }
 
-    targetPath = targetPath.replace(/([^/]$)/, '$1/').replace(/(^[^/])/, '/$1')
+  targetPath = targetPath.replace(/([^/]$)/, '$1/').replace(/(^[^/])/, '/$1')
 
-    const formData = {
-      dispatch: 'upload',
-      colId: '/',
-      ulUser: CONFIG.username, // back end record
-      siteId: '2',
-      colIdNormal: '/',
-      toDir: targetPath
-    }
+  const formData = {
+    dispatch: 'upload',
+    colId: '/',
+    ulUser: CONFIG.username, // back end record
+    siteId: '2',
+    colIdNormal: '/',
+    toDir: targetPath
+  }
 
-    const form = new FormData()
+  const form = new FormData()
 
-    Object.keys(formData).forEach(k => {
-      form.append(k, formData[k])
-    })
+  Object.keys(formData).forEach(k => {
+    form.append(k, formData[k])
+  })
 
-    // append files
-    Array.prototype.concat(file).forEach(f => form.append('ulfile', fs.createReadStream(f)))
+  // append files
+  Array.prototype.concat(file).forEach(f => form.append('ulfile', fs.createReadStream(f)))
 
-    function upload () {
-      return new Promise(async function (resolve, reject) {
-        let res
-        try {
-          res = await uploadClient.post('/Upload', {
-            body: form
-          })
-        } catch (err) {
-          res = err
-        }
-        return resolve(res.body)
-      })
-    }
-
-    console.log('开始上传...')
-
+  async function upload () {
+    let res
     try {
-      const body = await upload()
-      const reg = /<a[^>]*href="([^"]*)"[^>]*>(.*?)<\/a>/g
-      const uploadedFiles = body.match(reg)
-      uploadedFiles.forEach(function (item) {
-        const s = item.match(/>(.*)</)
-        console.log('已上传:', s[1])
-      })
-
-      console.log('已上传全部文件！')
-
-      return new Promise(function (resolve, reject) {
-        resolve()
+      res = await uploadClient.post('/Upload', {
+        body: form
       })
     } catch (err) {
-      reject(new Error(err))
+      if (err.statusCode === 302) {
+        res = err
+      } else {
+        throw err
+      }
     }
-  })
+    return res.body
+  }
+
+  console.log('开始上传...')
+
+  try {
+    const body = await upload()
+    const reg = /<a[^>]*href="([^"]*)"[^>]*>(.*?)<\/a>/g
+    const uploadedFiles = body.match(reg)
+    uploadedFiles.forEach(function (item) {
+      const s = item.match(/>(.*)</)
+      console.log('已上传:', s[1])
+    })
+
+    console.log('已上传全部文件！')
+
+    return true
+  } catch (err) {
+    throw err
+  }
 }
 
 exports.verifyUser = verifyUser
